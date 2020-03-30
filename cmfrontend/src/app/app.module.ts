590cd3427922b59dfeba14a898d30d68e1622b0f
Neythan Levans <neythan.astro@gmail.com>
2020-03-30 13:23:27 -0500

    mapeo de rutas desde el home en el componente navbar y en el componente homenews

--- cmfrontend/src/app/app.module.ts
+++ cmfrontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomenewsComponent } from './homenews/homenews.component';
+import { HomenewsComponent } from './home/homenews/homenews.component';
 import { HomeComponent } from './home/home.component';
 import { VisitasComponent } from './visitas/visitas.component';
 import {app_routing} from './app.routes';
